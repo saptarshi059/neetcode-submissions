a19a1486383feeb1175ab6065de310ba0af61321
saptarshi059 <45448695+saptarshi059@users.noreply.github.com>
2026-08-26 15:59:23 -0400

    Update: contains-duplicate-ii - submission-0

--- Data Structures & Algorithms/contains-duplicate-ii/submission-0.py
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.py
@@ -1,18 +1,17 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         window = set()
-        L = 0
-
-        for R in range(len(nums)):
-            # 1. Check for window validity
-            if R - L > k:
-                window.remove(nums[L])
-                L += 1
-
-            # 2. We have a valid window
-            if nums[R] in window:
-                return True
-            
-            window.add(nums[R])
+        i, j = 0, 0
+        while j < len(nums):
+            # 1. Check for valid window 
+            if abs(j - i) <= k:
+                if nums[j] in window:
+                    return True
+                window.add(nums[j])
+                j += 1
+            else:
+                # Window is invalid - thus, pop the leftmost element to make the window valid again
+                window.remove(nums[i])
+                i += 1
 
         return False

d48c25cf696ff55d3df49807dadefe371d5e842c
saptarshi059 <45448695+saptarshi059@users.noreply.github.com>
2026-08-29 15:22:52 -0400

    Update: maximum-frequency-stack - submission-1

--- Data Structures & Algorithms/maximum-frequency-stack/submission-1.py
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-1.py
@@ -1,38 +1,28 @@
-from collections import defaultdict
-
 class FreqStack:
+
     def __init__(self):
-        self.sos = defaultdict(list) # stack-of-stack (key = freq. level | val = stack at that freq. level).
-        self.freq = defaultdict(list) # frequencies of all elements.
-        self.max_cnt = 0 # highest frequency among all elements.
+        self.maxf = 0
+        self.counts = {} # Holds the overall frequency of elements.
+        self.stacks = defaultdict(list) # Holds individual stacks for frequency levels.
 
     def push(self, val: int) -> None:
-        self.freq[val] = 1 + self.freq.get(val, 0)
-        
-        # 1. If the frequency of the most recently added element is greater than previous max freq, update it.
-        if self.freq[val] > self.max_cnt:
-            self.max_cnt = self.freq[val]
+        # 1. Update frequency of element
+        self.counts[val] = 1 + self.counts.get(val, 0)
 
-        # 2. For this frequency level append to the stack
-        self.sos[self.freq[val]].append(val)
+        # 2. Update max_frequency
+        self.maxf = max(self.maxf, self.counts[val])
+
+        # 3. For that frequency level, push the val
+        self.stacks[self.counts[val]].append(val)
 
     def pop(self) -> int:
-        # 1. Find the stack associated with the max freq.
-        max_freq_stack = self.sos[self.max_cnt]
-
-        # 2. If the max_freq_stack is NOT empty, then pop the top from this stack and decrement count of the top element
-        if max_freq_stack:
-            top = max_freq_stack.pop()
-            self.freq[top] -= 1
-            return top
-        else:
-            # max_freq_stack is empty, so, decrease count first and then return from the next one.
-            self.max_cnt -= 1
-            next_best_stack = self.sos[self.max_cnt]
-            top = next_best_stack.pop()
-            self.freq[top] -= 1
-            return top
-
+        # 1. Check if the stack associated with maxf is empty - if it is, then reduce maxf by 1 and then pop from that level
+        if not self.stacks[self.maxf]:
+            self.maxf -= 1
+            
+        top = self.stacks[self.maxf].pop()
+        self.counts[top] -= 1
+        return top
 
 
 # Your FreqStack object will be instantiated and called as such:
